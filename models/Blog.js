eb70699904cc0df46c6b09c069eba404c815c07c
Sameer Ali <alisameer52718@gmail.com>
2025-06-28 14:00:05 +0500

    blogs half work done

--- models/Blog.js
+++ models/Blog.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import slugify from 'slugify';
 
 const blogSchema = new mongoose.Schema(
   {
@@ -7,22 +8,46 @@ const blogSchema = new mongoose.Schema(
     subCategoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'SubCategory', required: false },
 
     name: { type: String, required: true, trim: true },
+    slug: { type: String, unique: true, trim: true },
 
     blog: { type: String, required: true },
     image: { type: String, required: false, trim: true },
     shortDescription: { type: String, required: true, trim: true },
-    writterName: { type: String, required: true },
     tag: { type: [String], default: [] },
 
-    status: { type: Boolean, default: true},
-    isDeleted: { type: Boolean, default: false},
+    status: { type: Boolean, default: true },
+    isDeleted: { type: Boolean, default: false },
 
     wishList: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', default: [] },
-
-
   },
-  { timestamps: true }
+  { timestamps: true },
 );
+
+// Pre-save hook to generate unique slug
+blogSchema.pre('save', async function (next) {
+  if (!this.isModified('name')) {
+    return next();
+  }
+
+  // Generate base slug from name
+  let baseSlug = slugify(this.name, {
+    lower: true,
+    strict: true,
+    remove: /[*+~.()'"!:@]/g,
+  });
+
+  // Check for existing slugs and append counter if necessary
+  let slug = baseSlug;
+  let counter = 1;
+
+  while (await this.constructor.findOne({ slug, _id: { $ne: this._id } })) {
+    slug = `${baseSlug}-${counter}`;
+    counter++;
+  }
+
+  this.slug = slug;
+  next();
+});
 
 const Blog = mongoose.model('Blog', blogSchema);
 
