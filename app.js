42bc04af99553bc5cb2a0ae1326d6549fa8b9dd3
Sameer Ali <alisameer52718@gmail.com>
2025-09-28 18:34:54 +0500

    fix(cors): remove custom CORS options for simplified configuration

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('common'));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
