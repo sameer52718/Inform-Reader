b4ad35e17fd48d84e4b6c89b90658f73efb059b3
Sameer Ali <alisameer52718@gmail.com>
2025-10-15 15:58:39 +0500

    refactor(brand): enhance Brand model structure and improve category handling in specifications seeder

--- models/Brand.js
+++ models/Brand.js
@@ -4,10 +4,14 @@ const brandSchema = new mongoose.Schema(
   {
     adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     name: { type: String, required: true, unique: true },
-    status: { type: Boolean, default: true},
-    isDeleted: { type: Boolean, default: false},
+    status: { type: Boolean, default: true },
+    isDeleted: { type: Boolean, default: false },
+    category: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'Category',
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 const Brand = mongoose.model('Brand', brandSchema);
